b0274ffb3b93ef189fc7a2fd37368e0c258bbaad
Arunkumar Sivaramasethu <arunkumar1.s@intellectdesign.com>
2021-03-31 16:17:04 +0530

    Adding Idx Card component

---
+++ IdxCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-card class="idx-card extraction-card"
+                v-bind="$attrs"
+            >
+        <slot />
+
+    </v-card>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
